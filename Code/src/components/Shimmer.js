4beffa21fd2c6b8456384b7513a29b5ce90d60f4
Gowthami Sukhavasi <gowthami@Gowthamis-MacBook-Air.local>
2023-02-11 19:44:57 -0500

    Updated Shimmer UI

--- Code/src/components/Shimmer.js
+++ Code/src/components/Shimmer.js
@@ -1,5 +1,22 @@
+const CardShimmer = () => {
+  return (
+    <div className="shimmer-card">
+      <div className="shimmer-img stroke animate"></div>
+      <div className="shimmer-title stroke animate"></div>
+      <div className="shimmer-details stroke animate "></div>
+      <div className="shimmer-tags stroke animate "></div>
+    </div>
+  );
+};
+
 const Shimmer = () => {
-  return <h1> Shimmer UI loading ..............</h1>;
+  return (
+    <div className="shimmer-container">
+      {new Array(15).fill(0).map((element, index) => {
+        return <CardShimmer key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default Shimmer;
