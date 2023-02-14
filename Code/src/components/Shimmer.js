c172f980a8267ac973b7152c76159c6b3ff622cd
Gowthami Sukhavasi <gowthami@Gowthamis-MacBook-Air.local>
2023-02-13 21:10:25 -0500

    Modifying the Shimmer component

--- Code/src/components/Shimmer.js
+++ Code/src/components/Shimmer.js
@@ -19,4 +19,14 @@ const Shimmer = () => {
   );
 };
 
+{
+  /* <div className="restaurant-list">
+{Array(10)
+  .fill("")
+  .map((e, index) => (
+    <div key={index} className="shimmer-card"></div>
+  ))}
+</div> */
+}
+
 export default Shimmer;
